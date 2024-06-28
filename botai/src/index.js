c5f96388bd68b4bfdda62a35cc7c964484de2ede
Hemant391 <148564538+Hemant391@users.noreply.github.com>
2024-06-28 16:46:59 +0530

    handle empty history

--- botai/src/index.js
+++ botai/src/index.js
@@ -1,15 +1,4 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
 
-// <React.StrictMode>
-// <BrowserRouter>
-// <App />
-
-// </BrowserRouter>
-// </React.StrictMode>
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
